1bd2b1e86dad75176e12e4301201e3307c9826eb
jcstein <steinjc@vcu.edu>
2021-04-18 03:12:02 -0400

    initial commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,10 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/joshuastein/dev/jcs2/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/joshuastein/dev/jcs2/src/pages/404.js"))),
-  "component---src-pages-imprint-js": hot(preferDefault(require("/Users/joshuastein/dev/jcs2/src/pages/imprint.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/joshuastein/dev/jcs2/src/pages/index.js"))),
-  "component---src-pages-privacy-js": hot(preferDefault(require("/Users/joshuastein/dev/jcs2/src/pages/privacy.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/joshuastein/dev/PORTFOLIOS/jcs2/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/joshuastein/dev/PORTFOLIOS/jcs2/src/pages/404.js"))),
+  "component---src-pages-imprint-js": hot(preferDefault(require("/Users/joshuastein/dev/PORTFOLIOS/jcs2/src/pages/imprint.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/joshuastein/dev/PORTFOLIOS/jcs2/src/pages/index.js"))),
+  "component---src-pages-privacy-js": hot(preferDefault(require("/Users/joshuastein/dev/PORTFOLIOS/jcs2/src/pages/privacy.js")))
 }
 
