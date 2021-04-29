b03d87730459e484752196603a5413f2b20ee858
jcstein <steinjc@vcu.edu>
2021-04-29 10:57:09 -0400

    initial commit

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -15,7 +15,7 @@ var plugins = [{
       options: {"plugins":[],"googleAnalytics":{"trackingId":"G-NP4N9RF8TG","cookieName":"gatsby-gdpr-google-analytics","anonymize":true,"allowAdFeatures":false},"environments":["production"]},
     },{
       plugin: require('/Users/joshuastein/dev/PORTFOLIOS/jcs2/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"Josh Stein's Portfolio","short_name":"js.","start_url":"/","background_color":"#FFFFFF","theme_color":"#000000","display":"minimal-ui","icon":"content/favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"9b0f0d2fa7e1a87152756063bf4fa341"},
+      options: {"plugins":[],"name":"Josh Stein | Portfolio","short_name":"js.","start_url":"/","background_color":"#FFFFFF","theme_color":"#000000","display":"minimal-ui","icon":"content/favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"9b0f0d2fa7e1a87152756063bf4fa341"},
     },{
       plugin: require('/Users/joshuastein/dev/PORTFOLIOS/jcs2/node_modules/gatsby-plugin-mdx/gatsby-ssr'),
       options: {"plugins":[],"extensions":[".mdx",".md"],"gatsbyRemarkPlugins":[{"resolve":"gatsby-remark-images","options":{"maxWidth":1000,"quality":80}}],"defaultLayouts":{},"lessBabel":false,"remarkPlugins":[],"rehypePlugins":[],"mediaTypes":["text/markdown","text/x-markdown"]},
