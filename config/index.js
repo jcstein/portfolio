f4047f853bcdfec72e754926c95d7c9ea8819016
jcstein <mail@jcstein.dev>
2021-05-04 20:26:45 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   //-- SITE SETTINGS -----
   author: "Josh Stein",
   siteTitle: "Josh Stein | Portfolio",
-  siteShortTitle: "js.", // Used as logo text in header, footer, and splash screen
+  siteShortTitle: "js", // Used as logo text in header, footer, and splash screen
   siteDescription:
   "I'm a full-stack entrepreneur who specializes in lightweight, functional web design. I am currently learning build web experiences that are designed to reduce waste and help people around the globe.",
   siteUrl: "https://jcstein.dev",
